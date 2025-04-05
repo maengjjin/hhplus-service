a2311b7f4118e66c62eeedfdf8b0059ae8fda812
mj <kimmaengjin2@gmail.com>
2025-04-05 16:35:06 +0900

    - Swagger 문서 생성을 위한 어노테이션 적용 (controller)

--- src/main/java/kr/hhplus/be/server/coupon/contorller/CouponController.java
+++ src/main/java/kr/hhplus/be/server/coupon/contorller/CouponController.java
@@ -1,7 +1,14 @@
 package kr.hhplus.be.server.coupon.contorller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import kr.hhplus.be.server.common.ApiResult;
 import kr.hhplus.be.server.coupon.response.CouponResponse;
+import kr.hhplus.be.server.coupon.response.SwaggerCouponResponse;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/coupon")
 public class CouponController {
 
+    @Operation(summary = "쿠폰 발급 API", description = "쿠폰을 사용자에게 발급합니다",
+        responses = @ApiResponse(responseCode = "200", description = "success", content = @Content(schema = @Schema(implementation = SwaggerCouponResponse.class))))
+    @Parameters({@Parameter(name = "couponId", description = "쿠폰 ID", required = true)})
     @PostMapping("/send/{couponId}")
-    public ApiResult<CouponResponse> sendCoupon(@PathVariable long couponId) {
-        return ApiResult.success(CouponResponse.of(
-            1, "5,000원 할인쿠폰", 5000L, "20250501")
+    public ApiResult<CouponResponse> sendCoupon(
+        @Parameter(hidden = true) @PathVariable long couponId) {
+        return ApiResult.success(CouponResponse.of( 1, "5,000원 할인쿠폰", 5000L, "20250501")
         );
     }
 }
--- src/main/java/kr/hhplus/be/server/product/controller/ProductController.java
+++ src/main/java/kr/hhplus/be/server/product/controller/ProductController.java
@@ -1,9 +1,17 @@
 package kr.hhplus.be.server.product.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import java.util.List;
 import kr.hhplus.be.server.common.ApiResult;
 import kr.hhplus.be.server.product.response.ProductOptionResponse;
 import kr.hhplus.be.server.product.response.ProductResponse;
+import kr.hhplus.be.server.product.response.SwaggerProductOptionResponse;
+import kr.hhplus.be.server.product.response.SwaggerProductResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/product")
 public class ProductController {
 
+    @Operation(summary = "상품 단건 조회 API", description = "상품 단건 조회",
+        responses = @ApiResponse(responseCode = "200", description = "success", content = @Content(schema = @Schema(implementation = SwaggerProductOptionResponse.class))))
+    @Parameters({ @Parameter(name = "productId", description = "상품 ID", required = true)})
     @GetMapping("/{productId}")
-    public ApiResult<ProductResponse> getProduct(@PathVariable long productId) {
+    public ApiResult<ProductResponse> getProduct(@Parameter(hidden = true) @PathVariable long productId) {
         return ApiResult.success(ProductResponse.of(101L, "신발", 58000L, "상품 판매",
             List.of(
                 new ProductOptionResponse(1L, "흰색", 30000L, 20L),
@@ -23,6 +34,8 @@ public class ProductController {
         ));
     }
 
+    @Operation(summary = "상품 단건 조회 API", description = "상품 단건 조회",
+        responses = @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = SwaggerProductResponse.class))))
     @GetMapping("/ranking")
     public ApiResult<List<ProductResponse>> getProductRanking() {
         return ApiResult.success(
