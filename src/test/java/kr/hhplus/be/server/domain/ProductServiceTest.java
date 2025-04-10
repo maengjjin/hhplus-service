6a5ffda68409cfee26f4f4a69280f3acb438af8a
mj <kimmaengjin2@gmail.com>
2025-04-11 07:49:47 +0900

    - 테스트명 및 주석 수정

--- src/test/java/kr/hhplus/be/server/domain/ProductServiceTest.java
+++ src/test/java/kr/hhplus/be/server/domain/ProductServiceTest.java
@@ -11,7 +11,6 @@ import java.util.List;
 import kr.hhplus.be.server.Exception.ProductException.OutOfStockException;
 import kr.hhplus.be.server.Exception.ProductException.ProductInactiveException;
 import kr.hhplus.be.server.Exception.ProductException.ProductNotFoundException;
-import kr.hhplus.be.server.domain.order.OrderCommand;
 import kr.hhplus.be.server.domain.order.OrderItem;
 import kr.hhplus.be.server.domain.product.Product;
 import kr.hhplus.be.server.domain.product.ProductInfo;
@@ -22,7 +21,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -45,10 +43,6 @@ public class ProductServiceTest {
     ProductInfo productInfo;
 
 
-    // 상품 조회 성공
-    // 상품 조회 실패 (상품이 있거나, 상품상태 유효)
-    //
-
 
     @BeforeEach
     void beforeEach(){
@@ -70,19 +64,19 @@ public class ProductServiceTest {
 
 
     @Test
-    void 상품_조회_성공(){
+    void 상품_정보_조회_성공(){
 
-        // given 상품 조회를 성공 하기 위해 상품과 상품 옵션 세팅
+        // given 존재하는 상품 ID와 상품 옵션 설정
         long productId = 100L;
 
         Mockito.when(productRepository.findProductById(productId)).thenReturn(product);
         Mockito.when(productRepository.findProductOptionById(productId)).thenReturn(options);
 
 
-        // 상품 조회
+        // when 상품 조회
         ProductInfo result = productService.findProductInfo(productId);
 
-        // 상품 검증
+        // then 상품 검증
         Assertions.assertThat(result).usingRecursiveComparison().isEqualTo(productInfo);
         verify(productRepository, times(1)).findProductById(productId);
         verify(productRepository, times(1)).findProductOptionById(productId);
@@ -91,14 +85,14 @@ public class ProductServiceTest {
     }
 
     @Test
-    void 잘못된_상품을_조회_했을때(){
+    void 상품_조회시_상품이_없을때_예외발생(){
 
-        // given 상품 조회를 성공 하기 위해 상품과 상품 옵션 세팅
+        // given 존재하지 않는 상품 ID에 대해 productRepository가 null을 반환하도록 설정
         long productId = 200L;
 
         Mockito.when(productRepository.findProductById(productId)).thenReturn(null);
 
-
+        // when then 예외 발생
         assertThrows(ProductNotFoundException.class, () ->
             productService.findProductInfo(productId)
             );
@@ -106,22 +100,23 @@ public class ProductServiceTest {
     }
 
     @Test
-    void 상품_중단이_됐을때_예외() {
+    void 상품_조회시_중단된_상품일때_예외발생() {
 
-        // given
+        // given 상태가 중단된 상품을 반환하도록 설정
         product = Product.of(100L, "신발", 30_000L, INACTIVE);
 
         Mockito.when(productRepository.findProductById(100L)).thenReturn(product);
 
+        // when: 예외 발생
         assertThrows(ProductInactiveException.class, () ->
             productService.findProductInfo(100L));
 
     }
 
     @Test
-    void 상품_재고_조회_성공(){
+    void 상품_재고_충분할때_조회_성공(){
 
-        // given 상품, 상품옵션번호
+        // given 주문한 수량보다 재고가 충분한 상품옵션들이 존재함을 mock으로 설정
         List<OrderItem> orderItems = List.of(
             new OrderItem(100L, 101L, 2),
             new OrderItem(100L, 102L, 2)
@@ -133,7 +128,7 @@ public class ProductServiceTest {
         // when 상품 재고 조회
         productService.checkStockAvailability(orderItems);
 
-        // then 검증
+        // then 예외가 발생하지 않고, 메서드가 옵션 수만큼 호출되었는지 검증
         verify(productRepository, times(orderItems.size())).fetchOptionsByProductId(Mockito.anyLong(), Mockito.anyLong());
         assertDoesNotThrow(() ->
             productService.checkStockAvailability(orderItems)
@@ -142,16 +137,17 @@ public class ProductServiceTest {
     }
 
     @Test
-    void 상품_재고가_없을때_예외_발생(){
+    void 상품_재고가_요청수량보다_적을때_예외발생(){
 
-        // given 상품, 상품옵션번호
+        // given 한 상품옵션의 재고가 부족하도록 설정 (요청 수량 > 재고)
         List<OrderItem> orderItems = List.of(
-            new OrderItem(100L, 101L, 5),
-            new OrderItem(100L, 102L, 2)
+            new OrderItem(100L, 101L, 5)
         );
 
+        // when 재고 확인 요청
         when(productRepository.fetchOptionsByProductId(100L, 101L)).thenReturn(3L);
 
+        // then 예외 검증
         assertThrows(OutOfStockException.class, () ->
             productService.checkStockAvailability(orderItems)
         );
