130d72fca7138d0789a13156d17e34f85c489f60
mj <kimmaengjin2@gmail.com>
2025-04-18 09:54:30 +0900

    - 포인트 도메인 JPA 매핑, Repository 설정

--- src/main/java/kr/hhplus/be/server/infrastructure/point/PointJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/point/PointJpaRepository.java
@@ -2,9 +2,15 @@ package kr.hhplus.be.server.infrastructure.point;
 
 import kr.hhplus.be.server.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface PointJpaRepository extends JpaRepository<User, Long> {
 
+    @Modifying
+    @Query("UPDATE User u SET u.point = :point WHERE u.userId = :userId")
+    void updatePoint(@Param("userId") long userId, @Param("point") long point);
 
 }
--- src/main/java/kr/hhplus/be/server/infrastructure/point/PointRepositoryImpl.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/point/PointRepositoryImpl.java
@@ -3,22 +3,25 @@ package kr.hhplus.be.server.infrastructure.point;
 import kr.hhplus.be.server.domain.point.PointHistory;
 import kr.hhplus.be.server.domain.point.PointRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 
 
+@Component
 @RequiredArgsConstructor
-@Repository
-public class PointJpaRepositoryImpl implements PointRepository{
+public class PointRepositoryImpl implements PointRepository{
 
+    private final PointJpaRepository pointJpaRepository;
+
+    private final PointHistoryJpaRepository pointHistoryJpaRepository;
 
     @Override
     public void updatePoint(long userId, long amount) {
+        pointJpaRepository.updatePoint(userId, amount);
     }
-
-
 
     @Override
     public void savePointHistory(PointHistory entity) {
+        pointHistoryJpaRepository.save(entity);
 
     }
 }
