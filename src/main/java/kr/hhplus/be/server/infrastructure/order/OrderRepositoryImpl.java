663dc2ca62ec9728ad3230a536ce8794ca46536d
mj <kimmaengjin2@gmail.com>
2025-05-03 06:49:00 +0900

    fix: f2cfa427 커밋 누락 내용 보완

--- src/main/java/kr/hhplus/be/server/infrastructure/order/OrderRepositoryImpl.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/order/OrderRepositoryImpl.java
@@ -1,7 +1,7 @@
 package kr.hhplus.be.server.infrastructure.order;
 
 
-import java.util.Optional;
+import java.util.List;
 import kr.hhplus.be.server.domain.order.Order;
 import kr.hhplus.be.server.domain.order.OrderDetail;
 import kr.hhplus.be.server.domain.order.OrderRepository;
@@ -24,12 +24,7 @@ public class OrderRepositoryImpl implements OrderRepository {
     }
 
     @Override
-    public void saveOrderDetail(OrderDetail detail) {
-        orderDetailJpaRepository.save(detail);
-    }
-
-    @Override
-    public Optional<Order> findByOrderNoAndUserId(String orderNo, long userId) {
-        return orderJpaRepository.findByOrderNoAndUserId(orderNo, userId);
+    public void saveAll(List<OrderDetail> details) {
+        orderDetailJpaRepository.saveAll(details);
     }
 }
