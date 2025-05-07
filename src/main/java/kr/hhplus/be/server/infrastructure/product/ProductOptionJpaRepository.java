b2c7eec7b90fc8854d28aafb833d360ae162c6aa
mj <kimmaengjin2@gmail.com>
2025-05-07 23:15:54 +0900

    refactor: 사용하지 않는 쿼리 제거

--- src/main/java/kr/hhplus/be/server/infrastructure/product/ProductOptionJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/product/ProductOptionJpaRepository.java
@@ -5,7 +5,6 @@ import kr.hhplus.be.server.domain.product.ProductOption;
 import org.hibernate.PessimisticLockException;
 import org.springframework.dao.CannotAcquireLockException;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.retry.annotation.Backoff;
@@ -31,24 +30,6 @@ public interface ProductOptionJpaRepository extends JpaRepository<ProductOption,
              WHERE p.productId = :productId AND po.optionId = :optionId
              """)
     Optional<ProductOption> findOptionWithProduct(@Param("productId") long productId,@Param("optionId") long optionId);
-
-
-    @Modifying
-    @Query("""
-            UPDATE ProductOption po
-            SET po.stockQty = :stockQty
-            WHERE po.optionId = :optionId AND po.product.productId = :productId
-            """)
-    void updateStockQuantity(@Param("productId") long productId, @Param("optionId") long optionId,@Param("stockQty") long stockQty);
-
-
-    @Modifying
-    @Query("""
-            UPDATE ProductOption po
-            SET po.stockQty = :stockQty
-            WHERE po.optionId = :optionId AND po.product.productId = :productId
-            """)
-    int updateStockQuantity2(@Param("productId") long productId, @Param("optionId") long optionId,@Param("stockQty") long stockQty);
 
 
 
