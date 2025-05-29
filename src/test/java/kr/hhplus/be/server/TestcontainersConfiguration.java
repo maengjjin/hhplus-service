ad8ea0b644193f8958cc9d1e2c119fcf7700d986
mj <kimmaengjin2@gmail.com>
2025-05-30 06:36:59 +0900

    chore: 테스트 환경 설정 개선 및 임시 테스트 비활성화
    - 기존 컨테이너 기반 통합 테스트 주석 처리
    - 컨테이너 기반 설정 파일 통합 관리

--- src/test/java/kr/hhplus/be/server/TestcontainersConfiguration.java
+++ src/test/java/kr/hhplus/be/server/TestcontainersConfiguration.java
@@ -1,34 +1,68 @@
 package kr.hhplus.be.server;
 
 import jakarta.annotation.PreDestroy;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @Configuration
-class TestcontainersConfiguration {
+public class TestcontainersConfiguration {
 
-	public static final MySQLContainer<?> MYSQL_CONTAINER;
 
-	static {
-		MYSQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
-			.withDatabaseName("hhplus")
-			.withUsername("test")
-			.withPassword("test");
-		MYSQL_CONTAINER.start();
+    public static MySQLContainer<?> MYSQL_CONTAINER;
+    private static GenericContainer<?> REDIS_CONTAINER;
+    private static GenericContainer<?> KAFKA_CONTAINER;
 
-		System.setProperty("spring.datasource.url", MYSQL_CONTAINER.getJdbcUrl() + "?characterEncoding=UTF-8&serverTimezone=UTC");
-		System.setProperty("spring.datasource.username", MYSQL_CONTAINER.getUsername());
-		System.setProperty("spring.datasource.password", MYSQL_CONTAINER.getPassword());
-	}
+    static {
+        setMySQL();
+        setRedis();
+        setKafka();
+    }
 
-	@PreDestroy
-	public void preDestroy() {
-		if (MYSQL_CONTAINER.isRunning()) {
-			MYSQL_CONTAINER.stop();
-		}
-	}
+    private static void setKafka() {
+        KAFKA_CONTAINER = new GenericContainer<>(DockerImageName.parse("apache/kafka:4.0.0"))
+            .withExposedPorts(9092);  // KRaft 모드 기본 리스너 포트
+        KAFKA_CONTAINER.start();
+
+        String bootstrapServers =
+            KAFKA_CONTAINER.getHost() + ":" + KAFKA_CONTAINER.getFirstMappedPort();
+        System.setProperty("spring.kafka.bootstrap-servers", bootstrapServers);
+    }
+
+    private static void setRedis() {
+        REDIS_CONTAINER = new GenericContainer(DockerImageName.parse("redis:7.0.11-alpine"))
+            .withReuse(true)
+            .withExposedPorts(6379);
+        REDIS_CONTAINER.start();
+
+        System.setProperty("spring.data.redis.host", REDIS_CONTAINER.getHost());
+        System.setProperty("spring.data.redis.port", REDIS_CONTAINER.getFirstMappedPort().toString());
+    }
+
+    private static void setMySQL() {
+        MYSQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
+            .withDatabaseName("hhplus")
+            .withUsername("test")
+            .withPassword("test");
+        MYSQL_CONTAINER.start();
+
+        System.setProperty("spring.datasource.url", MYSQL_CONTAINER.getJdbcUrl() + "?characterEncoding=UTF-8&serverTimezone=UTC");
+        System.setProperty("spring.datasource.username", MYSQL_CONTAINER.getUsername());
+        System.setProperty("spring.datasource.password", MYSQL_CONTAINER.getPassword());
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        if (MYSQL_CONTAINER.isRunning()) {
+            MYSQL_CONTAINER.stop();
+        }
+        if (REDIS_CONTAINER.isRunning()) {
+            REDIS_CONTAINER.stop();
+        }
+
+        if(KAFKA_CONTAINER.isRunning()) {
+            KAFKA_CONTAINER.stop();
+        }
+    }
 }
--- src/test/java/kr/hhplus/be/server/infrastructure/order/OrderJpaRepositoryTest.java
+++ src/test/java/kr/hhplus/be/server/infrastructure/order/OrderJpaRepositoryTest.java
@@ -1,60 +1,60 @@
-package kr.hhplus.be.server.infrastructure.order;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-import jakarta.persistence.EntityManager;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
-import kr.hhplus.be.server.domain.order.OrderDetail;
-import kr.hhplus.be.server.domain.statistics.ProductOrderVolume;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.transaction.annotation.Transactional;
-
-@Transactional
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class OrderJpaRepositoryTest {
-
-
-    @Autowired
-    OrderDetailJpaRepository orderDetailJpaRepository;
-
-    @Autowired
-    EntityManager em;
-
-    @Test
-    void 상품과_옵션_연관_조회_정상_수행() {
-        // given 상품과 옵션을 저장하고 양방향 연관관계 연결
-        OrderDetail orderDetail = new OrderDetail(1, 1, 1, 2000, 10);
-
-        OrderDetail orderDetail2 = new OrderDetail(1, 1, 1, 2000, 10);
-
-        OrderDetail orderDetail3 = new OrderDetail(1, 4, 1, 2000, 15);
-
-        List<OrderDetail> orderDetailList = List.of(orderDetail, orderDetail2, orderDetail3);
-        orderDetailJpaRepository.saveAll(orderDetailList);
-
-        em.flush(); // DB 반영
-        em.clear(); // 영속성 컨텍스트 초기화 → 진짜 DB에서 조회됨
-
-        LocalDate date = LocalDate.now();
-        LocalDateTime start = date.atStartOfDay();
-        LocalDateTime end = date.plusDays(1).atStartOfDay();
-
-        List<ProductOrderVolume> result = orderDetailJpaRepository.findAggregateTopOrders(start, end);
-
-        Assertions.assertThat(result).hasSize(2);
-        assertThat(result.get(0).getOrderQty()).isEqualTo(20);
-        assertThat(result.get(1).getOrderQty()).isEqualTo(15); // 예시 수량
-
-
-
-
-    }
-
-}
+//package kr.hhplus.be.server.infrastructure.order;
+//
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+//
+//import jakarta.persistence.EntityManager;
+//import java.time.LocalDate;
+//import java.time.LocalDateTime;
+//import java.util.List;
+//import kr.hhplus.be.server.domain.order.OrderDetail;
+//import kr.hhplus.be.server.domain.statistics.ProductOrderVolume;
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@Transactional
+//@DataJpaTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//public class OrderJpaRepositoryTest {
+//
+//
+//    @Autowired
+//    OrderDetailJpaRepository orderDetailJpaRepository;
+//
+//    @Autowired
+//    EntityManager em;
+//
+//    @Test
+//    void 상품과_옵션_연관_조회_정상_수행() {
+//        // given 상품과 옵션을 저장하고 양방향 연관관계 연결
+//        OrderDetail orderDetail = new OrderDetail(1, 1, 1, 2000, 10);
+//
+//        OrderDetail orderDetail2 = new OrderDetail(1, 1, 1, 2000, 10);
+//
+//        OrderDetail orderDetail3 = new OrderDetail(1, 4, 1, 2000, 15);
+//
+//        List<OrderDetail> orderDetailList = List.of(orderDetail, orderDetail2, orderDetail3);
+//        orderDetailJpaRepository.saveAll(orderDetailList);
+//
+//        em.flush(); // DB 반영
+//        em.clear(); // 영속성 컨텍스트 초기화 → 진짜 DB에서 조회됨
+//
+//        LocalDate date = LocalDate.now();
+//        LocalDateTime start = date.atStartOfDay();
+//        LocalDateTime end = date.plusDays(1).atStartOfDay();
+//
+//        List<ProductOrderVolume> result = orderDetailJpaRepository.findAggregateTopOrders(start, end);
+//
+//        Assertions.assertThat(result).hasSize(2);
+//        assertThat(result.get(0).getOrderQty()).isEqualTo(20);
+//        assertThat(result.get(1).getOrderQty()).isEqualTo(15); // 예시 수량
+//
+//
+//
+//
+//    }
+//
+//}
